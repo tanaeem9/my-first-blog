c8aa16e0ccd4cfbebb10ea72d1a81369e853dc5a
tanaeem9 <tanaeem9@gmail.com>
2024-08-11 14:06:20 +0500

    My Django Girls app, first commit

--- forms/views.py
+++ forms/views.py
@@ -4,4 +4,4 @@ from .models import Form
 # Create your views here.
 def post_list(request):    
     forms = Form.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'forms/post_list.html', {'posts': forms})
+    return render(request, 'forms/post_list.html', {'forms': forms})
