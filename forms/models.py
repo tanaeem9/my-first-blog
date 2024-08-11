5c5325f3d791ffaa90ffd89f428d4dc639d5540f
tanaeem9 <tanaeem9@gmail.com>
2024-08-11 12:48:55 +0500

    My Django Girls app, first commit

--- forms/models.py
+++ forms/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
-class Forms(models.Model):
+class Form(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
